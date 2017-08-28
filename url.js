702a5050ba3e9c175216f90b5d69d615675d4478
Moatilliatta <murdoc_87_1@hotmail.com>
2017-08-28 13:24:17 -0500

    Updated code to strict standars
    File ready to be added to gruntfile whatsoever, it respects the strict standars

--- url.js
+++ url.js
@@ -14,8 +14,9 @@
     } catch(e) {}
   }
 
-  if (hasWorkingUrl)
+  if (hasWorkingUrl) {
     return;
+  }
 
   var relative = Object.create(null);
   relative['ftp'] = 21;
@@ -33,23 +34,23 @@
   relativePathDotMapping['%2e%2e'] = '..';
 
   function isRelativeScheme(scheme) {
-    return relative[scheme] !== undefined;
+    return typeof relative[scheme] !== "undefined";
   }
 
-  function invalid() {
+  var invalid = function () {
     clear.call(this);
     this._isInvalid = true;
-  }
+  };
 
-  function IDNAToASCII(h) {
-    if ('' == h) {
-      invalid.call(this)
+  var IDNAToASCII = function (h) {
+    if ('' === h) {
+      invalid.call(this);
     }
     // XXX
-    return h.toLowerCase()
-  }
+    return h.toLowerCase();
+  };
 
-  function percentEscape(c) {
+  var percentEscape = function (c) {
     var unicode = c.charCodeAt(0);
     if (unicode > 0x20 &&
        unicode < 0x7F &&
@@ -59,9 +60,9 @@
       return c;
     }
     return encodeURIComponent(c);
-  }
+  };
 
-  function percentEscapeQuery(c) {
+  var percentEscapeQuery = function (c) {
     // XXX This actually needs to encode c using encoding and then
     // convert the bytes one-by-one.
 
@@ -74,15 +75,15 @@
       return c;
     }
     return encodeURIComponent(c);
-  }
+  };
 
-  var EOF = undefined,
-      ALPHA = /[a-zA-Z]/,
+  var ALPHA = /[a-zA-Z]/,
       ALPHANUMERIC = /[a-zA-Z0-9\+\-\.]/;
 
-  function parse(input, stateOverride, base) {
+  var parse = function (input, stateOverride, base) {
+    
     function err(message) {
-      errors.push(message)
+      errors.push(message);
     }
 
     var state = stateOverride || 'scheme start',
@@ -92,7 +93,7 @@
         seenBracket = false,
         errors = [];
 
-    loop: while ((input[cursor - 1] != EOF || cursor == 0) && !this._isInvalid) {
+    loop: while (( typeof input[cursor - 1] !== "undefined" || cursor === 0) && !this._isInvalid) {
       var c = input[cursor];
       switch (state) {
         case 'scheme start':
@@ -135,10 +136,10 @@
             cursor = 0;
             state = 'no scheme';
             continue;
-          } else if (EOF == c) {
+          } else if ( typeof c === "undefined" ) {
             break loop;
           } else {
-            err('Code point not allowed in scheme: ' + c)
+            err('Code point not allowed in scheme: ' + c);
             break loop;
           }
           break;
@@ -152,7 +153,7 @@
             state = 'fragment';
           } else {
             // XXX error handling
-            if (EOF != c && '\t' != c && '\n' != c && '\r' != c) {
+            if ( typeof c !== "undefined" && '\t' != c && '\n' != c && '\r' != c) {
               this._schemeData += percentEscape(c);
             }
           }
@@ -174,15 +175,16 @@
           } else {
             err('Expected /, got: ' + c);
             state = 'relative';
-            continue
+            continue;
           }
           break;
 
         case 'relative':
           this._isRelative = true;
-          if ('file' != this._scheme)
+          if ('file' != this._scheme){
             this._scheme = base._scheme;
-          if (EOF == c) {
+          }
+          if ( typeof c === "undefined" ) {
             this._host = base._host;
             this._port = base._port;
             this._path = base._path.slice();
@@ -191,8 +193,9 @@
             this._password = base._password;
             break loop;
           } else if ('/' == c || '\\' == c) {
-            if ('\\' == c)
+            if ('\\' == c){
               err('\\ is an invalid code point.');
+            }
             state = 'relative slash';
           } else if ('?' == c) {
             this._host = base._host;
@@ -212,12 +215,12 @@
             this._password = base._password;
             state = 'fragment';
           } else {
-            var nextC = input[cursor+1]
-            var nextNextC = input[cursor+2]
+            var nextC = input[cursor+1];
+            var nextNextC = input[cursor+2];
             if (
               'file' != this._scheme || !ALPHA.test(c) ||
               (nextC != ':' && nextC != '|') ||
-              (EOF != nextNextC && '/' != nextNextC && '\\' != nextNextC && '?' != nextNextC && '#' != nextNextC)) {
+              (  typeof nextNextC !== "undefined" && '/' != nextNextC && '\\' != nextNextC && '?' != nextNextC && '#' != nextNextC)) {
               this._host = base._host;
               this._port = base._port;
               this._username = base._username;
@@ -298,10 +301,14 @@
                 continue;
               }
               var tempC = percentEscape(cp);
-              (null !== this._password) ? this._password += tempC : this._username += tempC;
+              if (null !== this._password) {
+                this._password += tempC; 
+              } else {
+                this._username += tempC;
+              }
             }
             buffer = '';
-          } else if (EOF == c || '/' == c || '\\' == c || '?' == c || '#' == c) {
+          } else if ( typeof c === "undefined" || '/' == c || '\\' == c || '?' == c || '#' == c) {
             cursor -= buffer.length;
             buffer = '';
             state = 'host';
@@ -312,10 +319,10 @@
           break;
 
         case 'file host':
-          if (EOF == c || '/' == c || '\\' == c || '?' == c || '#' == c) {
+          if ( typeof c === "undefined" || '/' == c || '\\' == c || '?' == c || '#' == c) {
             if (buffer.length == 2 && ALPHA.test(buffer[0]) && (buffer[1] == ':' || buffer[1] == '|')) {
               state = 'relative path';
-            } else if (buffer.length == 0) {
+            } else if (buffer.length === 0) {
               state = 'relative path start';
             } else {
               this._host = IDNAToASCII.call(this, buffer);
@@ -340,7 +347,7 @@
             if ('hostname' == stateOverride) {
               break loop;
             }
-          } else if (EOF == c || '/' == c || '\\' == c || '?' == c || '#' == c) {
+          } else if ( typeof c === "undefined" || '/' == c || '\\' == c || '?' == c || '#' == c) {
             this._host = IDNAToASCII.call(this, buffer);
             buffer = '';
             state = 'relative path start';
@@ -363,8 +370,8 @@
         case 'port':
           if (/[0-9]/.test(c)) {
             buffer += c;
-          } else if (EOF == c || '/' == c || '\\' == c || '?' == c || '#' == c || stateOverride) {
-            if ('' != buffer) {
+          } else if ( typeof c === "undefined" || '/' == c || '\\' == c || '?' == c || '#' == c || stateOverride) {
+            if ('' !== buffer) {
               var temp = parseInt(buffer, 10);
               if (temp != relative[this._scheme]) {
                 this._port = temp + '';
@@ -384,16 +391,20 @@
           break;
 
         case 'relative path start':
-          if ('\\' == c)
+          
+          if ('\\' == c){
             err("'\\' not allowed in path.");
+          }
+          
           state = 'relative path';
+          
           if ('/' != c && '\\' != c) {
             continue;
           }
           break;
 
         case 'relative path':
-          if (EOF == c || '/' == c || '\\' == c || (!stateOverride && ('?' == c || '#' == c))) {
+          if ( typeof c === "undefined" || '/' == c || '\\' == c || (!stateOverride && ('?' == c || '#' == c))) {
             if ('\\' == c) {
               err('\\ not allowed in relative path.');
             }
@@ -409,7 +420,7 @@
             } else if ('.' == buffer && '/' != c && '\\' != c) {
               this._path.push('');
             } else if ('.' != buffer) {
-              if ('file' == this._scheme && this._path.length == 0 && buffer.length == 2 && ALPHA.test(buffer[0]) && buffer[1] == '|') {
+              if ('file' == this._scheme && this._path.length === 0 && buffer.length == 2 && ALPHA.test(buffer[0]) && buffer[1] == '|') {
                 buffer = buffer[0] + ':';
               }
               this._path.push(buffer);
@@ -431,13 +442,13 @@
           if (!stateOverride && '#' == c) {
             this._fragment = '#';
             state = 'fragment';
-          } else if (EOF != c && '\t' != c && '\n' != c && '\r' != c) {
+          } else if ( typeof c !== "undefined" && '\t' != c && '\n' != c && '\r' != c) {
             this._query += percentEscapeQuery(c);
           }
           break;
 
         case 'fragment':
-          if (EOF != c && '\t' != c && '\n' != c && '\r' != c) {
+          if ( typeof c !== "undefined" && '\t' != c && '\n' != c && '\r' != c) {
             this._fragment += c;
           }
           break;
@@ -445,9 +456,9 @@
 
       cursor++;
     }
-  }
+  };
 
-  function clear() {
+  var clear = function () {
     this._scheme = '';
     this._schemeData = '';
     this._username = '';
@@ -459,13 +470,14 @@
     this._fragment = '';
     this._isInvalid = false;
     this._isRelative = false;
-  }
+  };
 
   // Does not process domain names or IP addresses.
   // Does not handle encoding for the query parameter.
-  function jURL(url, base /* , encoding */) {
-    if (base !== undefined && !(base instanceof jURL))
-      base = new jURL(String(base));
+  var Jurl = function (url, base /* , encoding */) {
+    if ( typeof base !== "undefined" && !(base instanceof Jurl)) {
+      base = new Jurl(String(base));
+    }
 
     this._url = url;
     clear.call(this);
@@ -474,20 +486,21 @@
     // encoding = encoding || 'utf-8'
 
     parse.call(this, input, null, base);
-  }
+  };
 
-  jURL.prototype = {
+  Jurl.prototype = {
     toString: function() {
       return this.href;
     },
     get href() {
-      if (this._isInvalid)
+      if (this._isInvalid){
         return this._url;
+      }
 
       var authority = '';
-      if ('' != this._username || null != this._password) {
+      if ('' !== this._username || null !== this._password) {
         authority = this._username +
-            (null != this._password ? ':' + this._password : '') + '@';
+            (null !== this._password ? ':' + this._password : '') + '@';
       }
 
       return this.protocol +
@@ -503,8 +516,10 @@
       return this._scheme + ':';
     },
     set protocol(protocol) {
-      if (this._isInvalid)
+      if (this._isInvalid) {
         return;
+      }
+
       parse.call(this, protocol + ':', 'scheme start');
     },
 
@@ -513,8 +528,9 @@
           this._host + ':' + this._port : this._host;
     },
     set host(host) {
-      if (this._isInvalid || !this._isRelative)
+      if (this._isInvalid || !this._isRelative) {
         return;
+      }
       parse.call(this, host, 'host');
     },
 
@@ -522,8 +538,10 @@
       return this._host;
     },
     set hostname(hostname) {
-      if (this._isInvalid || !this._isRelative)
+      if (this._isInvalid || !this._isRelative) {
         return;
+      }
+
       parse.call(this, hostname, 'hostname');
     },
 
@@ -531,8 +549,10 @@
       return this._port;
     },
     set port(port) {
-      if (this._isInvalid || !this._isRelative)
+      if (this._isInvalid || !this._isRelative) {
         return;
+      }
+
       parse.call(this, port, 'port');
     },
 
@@ -541,8 +561,10 @@
           '/' + this._path.join('/') : this._schemeData;
     },
     set pathname(pathname) {
-      if (this._isInvalid || !this._isRelative)
+      if (this._isInvalid || !this._isRelative) {
         return;
+      }
+
       this._path = [];
       parse.call(this, pathname, 'relative path start');
     },
@@ -552,11 +574,16 @@
           '' : this._query;
     },
     set search(search) {
-      if (this._isInvalid || !this._isRelative)
+      if (this._isInvalid || !this._isRelative) {
         return;
+      }
+
       this._query = '?';
-      if ('?' == search[0])
+      
+      if ('?' == search[0]) {
         search = search.slice(1);
+      }
+
       parse.call(this, search, 'query');
     },
 
@@ -565,11 +592,16 @@
           '' : this._fragment;
     },
     set hash(hash) {
-      if (this._isInvalid)
+      if (this._isInvalid){
         return;
+      }
+
       this._fragment = '#';
-      if ('#' == hash[0])
+      
+      if ('#' == hash[0]){
         hash = hash.slice(1);
+      }
+
       parse.call(this, hash, 'fragment');
     },
 
@@ -601,16 +633,16 @@
   // Copy over the static methods
   var OriginalURL = scope.URL;
   if (OriginalURL) {
-    jURL.createObjectURL = function(blob) {
+    Jurl.createObjectURL = function(blob) {
       // IE extension allows a second optional options argument.
       // http://msdn.microsoft.com/en-us/library/ie/hh772302(v=vs.85).aspx
       return OriginalURL.createObjectURL.apply(OriginalURL, arguments);
     };
-    jURL.revokeObjectURL = function(url) {
+    Jurl.revokeObjectURL = function(url) {
       OriginalURL.revokeObjectURL(url);
     };
   }
 
-  scope.URL = jURL;
+  scope.URL = Jurl;
 
 })(window);
